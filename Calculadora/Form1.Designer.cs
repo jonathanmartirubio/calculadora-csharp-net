bec926d1c8d7b851ffca157cb0454f6f165d4363
jonathanmartirubio <jonamr09@gmail.com>
2022-06-17 20:04:57 +0200

    Botones creados

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -29,13 +29,336 @@ namespace Calculadora
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbVentana = new System.Windows.Forms.TextBox();
+            this.bResto = new System.Windows.Forms.Button();
+            this.bBorrar = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.bDividir = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.bMultiplicar = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.bRestar = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.bComa = new System.Windows.Forms.Button();
+            this.bResultado = new System.Windows.Forms.Button();
+            this.bCambiarSigno = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.bSumar = new System.Windows.Forms.Button();
+            this.bAlCuadrado = new System.Windows.Forms.Button();
+            this.bRaiz = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbVentana
+            // 
+            this.tbVentana.Location = new System.Drawing.Point(12, 29);
+            this.tbVentana.Multiline = true;
+            this.tbVentana.Name = "tbVentana";
+            this.tbVentana.Size = new System.Drawing.Size(356, 46);
+            this.tbVentana.TabIndex = 0;
+            // 
+            // bResto
+            // 
+            this.bResto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bResto.Location = new System.Drawing.Point(12, 94);
+            this.bResto.Name = "bResto";
+            this.bResto.Size = new System.Drawing.Size(137, 53);
+            this.bResto.TabIndex = 1;
+            this.bResto.Text = "%";
+            this.bResto.UseVisualStyleBackColor = true;
+            // 
+            // bBorrar
+            // 
+            this.bBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bBorrar.Location = new System.Drawing.Point(155, 94);
+            this.bBorrar.Name = "bBorrar";
+            this.bBorrar.Size = new System.Drawing.Size(67, 53);
+            this.bBorrar.TabIndex = 1;
+            this.bBorrar.Text = "C";
+            this.bBorrar.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(301, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 53);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "X²";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // b7
+            // 
+            this.b7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b7.Location = new System.Drawing.Point(12, 153);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(64, 53);
+            this.b7.TabIndex = 1;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            // 
+            // b8
+            // 
+            this.b8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b8.Location = new System.Drawing.Point(82, 153);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(67, 53);
+            this.b8.TabIndex = 1;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            // 
+            // b9
+            // 
+            this.b9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b9.Location = new System.Drawing.Point(155, 153);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(67, 53);
+            this.b9.TabIndex = 1;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            // 
+            // bDividir
+            // 
+            this.bDividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bDividir.Location = new System.Drawing.Point(228, 153);
+            this.bDividir.Name = "bDividir";
+            this.bDividir.Size = new System.Drawing.Size(67, 53);
+            this.bDividir.TabIndex = 1;
+            this.bDividir.Text = "/";
+            this.bDividir.UseVisualStyleBackColor = true;
+            // 
+            // b4
+            // 
+            this.b4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b4.Location = new System.Drawing.Point(12, 212);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(64, 53);
+            this.b4.TabIndex = 1;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            // 
+            // b5
+            // 
+            this.b5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b5.Location = new System.Drawing.Point(82, 212);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(67, 53);
+            this.b5.TabIndex = 1;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            // 
+            // b6
+            // 
+            this.b6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b6.Location = new System.Drawing.Point(155, 212);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(67, 53);
+            this.b6.TabIndex = 1;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            // 
+            // bMultiplicar
+            // 
+            this.bMultiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bMultiplicar.Location = new System.Drawing.Point(228, 212);
+            this.bMultiplicar.Name = "bMultiplicar";
+            this.bMultiplicar.Size = new System.Drawing.Size(67, 53);
+            this.bMultiplicar.TabIndex = 1;
+            this.bMultiplicar.Text = "*";
+            this.bMultiplicar.UseVisualStyleBackColor = true;
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(12, 271);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(64, 53);
+            this.b1.TabIndex = 1;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(82, 271);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(67, 53);
+            this.b2.TabIndex = 1;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(155, 271);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(67, 53);
+            this.b3.TabIndex = 1;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            // 
+            // bRestar
+            // 
+            this.bRestar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bRestar.Location = new System.Drawing.Point(228, 271);
+            this.bRestar.Name = "bRestar";
+            this.bRestar.Size = new System.Drawing.Size(67, 53);
+            this.bRestar.TabIndex = 1;
+            this.bRestar.Text = "-";
+            this.bRestar.UseVisualStyleBackColor = true;
+            // 
+            // b0
+            // 
+            this.b0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b0.Location = new System.Drawing.Point(12, 330);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(137, 53);
+            this.b0.TabIndex = 1;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            // 
+            // bComa
+            // 
+            this.bComa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bComa.Location = new System.Drawing.Point(155, 330);
+            this.bComa.Name = "bComa";
+            this.bComa.Size = new System.Drawing.Size(67, 53);
+            this.bComa.TabIndex = 1;
+            this.bComa.Text = ".";
+            this.bComa.UseVisualStyleBackColor = true;
+            // 
+            // bResultado
+            // 
+            this.bResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bResultado.Location = new System.Drawing.Point(301, 212);
+            this.bResultado.Name = "bResultado";
+            this.bResultado.Size = new System.Drawing.Size(67, 171);
+            this.bResultado.TabIndex = 1;
+            this.bResultado.Text = "=";
+            this.bResultado.UseVisualStyleBackColor = true;
+            // 
+            // bCambiarSigno
+            // 
+            this.bCambiarSigno.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCambiarSigno.Location = new System.Drawing.Point(228, 94);
+            this.bCambiarSigno.Name = "bCambiarSigno";
+            this.bCambiarSigno.Size = new System.Drawing.Size(67, 53);
+            this.bCambiarSigno.TabIndex = 1;
+            this.bCambiarSigno.Text = "+/-";
+            this.bCambiarSigno.UseVisualStyleBackColor = true;
+            // 
+            // button21
+            // 
+            this.button21.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button21.Location = new System.Drawing.Point(228, 330);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(67, 53);
+            this.button21.TabIndex = 1;
+            this.button21.Text = ".";
+            this.button21.UseVisualStyleBackColor = true;
+            // 
+            // bSumar
+            // 
+            this.bSumar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bSumar.Location = new System.Drawing.Point(228, 330);
+            this.bSumar.Name = "bSumar";
+            this.bSumar.Size = new System.Drawing.Size(67, 53);
+            this.bSumar.TabIndex = 1;
+            this.bSumar.Text = "+";
+            this.bSumar.UseVisualStyleBackColor = true;
+            // 
+            // bAlCuadrado
+            // 
+            this.bAlCuadrado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bAlCuadrado.Location = new System.Drawing.Point(301, 94);
+            this.bAlCuadrado.Name = "bAlCuadrado";
+            this.bAlCuadrado.Size = new System.Drawing.Size(67, 53);
+            this.bAlCuadrado.TabIndex = 1;
+            this.bAlCuadrado.Text = "X²";
+            this.bAlCuadrado.UseVisualStyleBackColor = true;
+            // 
+            // bRaiz
+            // 
+            this.bRaiz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bRaiz.Location = new System.Drawing.Point(301, 153);
+            this.bRaiz.Name = "bRaiz";
+            this.bRaiz.Size = new System.Drawing.Size(67, 53);
+            this.bRaiz.TabIndex = 1;
+            this.bRaiz.Text = "√";
+            this.bRaiz.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(380, 395);
+            this.Controls.Add(this.bResultado);
+            this.Controls.Add(this.bRestar);
+            this.Controls.Add(this.bMultiplicar);
+            this.Controls.Add(this.bDividir);
+            this.Controls.Add(this.bRaiz);
+            this.Controls.Add(this.bAlCuadrado);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.bSumar);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.bComa);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.bBorrar);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.bCambiarSigno);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.bResto);
+            this.Controls.Add(this.tbVentana);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbVentana;
+        private System.Windows.Forms.Button bResto;
+        private System.Windows.Forms.Button bBorrar;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button bDividir;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button bMultiplicar;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button bRestar;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button bComa;
+        private System.Windows.Forms.Button bResultado;
+        private System.Windows.Forms.Button bCambiarSigno;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button bSumar;
+        private System.Windows.Forms.Button bAlCuadrado;
+        private System.Windows.Forms.Button bRaiz;
     }
 }
 
